43fd15cb3c4e53a4ffc7578273b3f3f852fee3aa
tuzmusic <tuzmusic@gmail.com>
2019-08-13 21:14:41 -0400

    add some more tests

--- __tests__/NewCustomerScreen.test.js
+++ __tests__/NewCustomerScreen.test.js
@@ -3,9 +3,24 @@ import { NewCustomerScreen } from "../src/screens/NewCustomerScreen";
 import { render, fireEvent } from "react-native-testing-library";
 
 describe("NewCustomerScreen", () => {
-  const { getByText } = render(<NewCustomerScreen />);
+  const component = render(<NewCustomerScreen />);
   it("renders the screen title", () => {
-    expect(getByText("New Customer")).toBeDefined();
-    expect(true).toBe(true);
+    expect(component.getByText("New Customer")).toBeDefined();
+    expect(component.queryByText("Not there")).toBeNull();
+  });
+
+  it("has fields for name, description, and address", () => {
+    expect(component.getByPlaceholder("Name")).toBeDefined();
+    expect(component.getByPlaceholder("Address")).toBeDefined();
+    expect(component.getByPlaceholder("Description")).toBeDefined();
+  });
+
+  it("updates the state from the name and description fields", () => {
+    const nameField = component.getByPlaceholder("Name");
+    const descriptionField = component.getByPlaceholder("Description");
+    fireEvent.changeText(nameField, "John Doe");
+    expect(nameField.props.value).toEqual("John Doe");
+    fireEvent.changeText(descriptionField, "Some guy");
+    expect(descriptionField.props.value).toEqual("Some guy");
   });
 });
